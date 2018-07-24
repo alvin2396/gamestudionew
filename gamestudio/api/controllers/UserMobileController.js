6a37653ec1b9e209c1fd43374a99144255ed7f2d
alvin2396 <alvin2396@yahoo.com>
2018-07-24 15:57:53 +0700

    backup kantor

--- gamestudio/api/controllers/UserMobileController.js
+++ gamestudio/api/controllers/UserMobileController.js
@@ -10,29 +10,37 @@ module.exports = {
         var email = req.param('email');
         var password = req.param('password');
     
-        if(!email || !password){
-          return res.json(401,{err: 'email and password required'});
+        if (!email || !password) {
+          return res.json(401, {err: 'email and password required'});
         }
-        User.findOne({email : email}),function(err, user){
-          if(!user){
-            return res.json(401, {err: 'email and password invalid'});
+    
+        User.findOne({email: email}, function (err, user) {
+          if (!user) {
+            return res.json(401, {err: 'invalid email or password'});
           }
-           User.comparePassword(password, user, function(err, valid){
-             if(err){
-               return res.json(403, {err : 'forbidden'});
-             }
-             else{
-                if(!valid){
-                  return res.json(401, {err: 'email and password invalid'});
-                }
-                else{
-                  res.json({
-                    user : user,
-                  })
-                }
-             }
-           })
-        }
+    
+          User.comparePassword(password, user, function (err, valid) {
+            if (err) {
+              return res.json(403, {err: 'forbidden'});
+            }
+    
+            if (!valid) {
+              return res.json(401, {err: 'invalid email or password'});
+            } else {
+              user.jwTString=[]
+              user.jwTString.push({
+                id: user.id,
+                nama : user.nama,
+                token: jwToken.issue({id : user.id })
+
+              })
+              res.json({
+                user: user,
+                token: jwToken.issue({id : user.id })
+              });
+            }
+          });
+        })
       }
 
 };
